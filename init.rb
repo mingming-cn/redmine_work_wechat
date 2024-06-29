cac408adb3c54dd11dcbb5a016298b31960cb8ff
mingming <contact@mingming.wang>
2024-06-29 20:29:54 +0800

    refactor(init): update version

--- init.rb
+++ init.rb
@@ -1,12 +1,11 @@
 require 'redmine'
 require_relative 'lib/redmine_work_wechat'
 
-
 Redmine::Plugin.register :redmine_work_wechat do
   name 'Redmine Work Wechat plugin'
   author 'mingming.wang'
   description 'This is a plugin of Work Wechat for Redmine'
-  version '0.0.1'
+  version '0.0.3'
   url 'https://github.com/mingming-cn/redmine_work_wechat'
   author_url 'https://mingming.wang'
 
